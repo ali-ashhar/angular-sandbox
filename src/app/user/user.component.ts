bd7e5a6bddd07071cec760c6e3558be21f805a74
ali-ashhar <aliashhar021@gmail.com>
2024-07-19 22:42:04 +0330

    add users dummy add pictuer folder add getter function to user comp

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,42 +1,18 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 
-export const DUMMY_USERS = [
-  {
-    id: 'u1',
-    name: 'Jasmine Washington',
-    avatar: 'user-1.jpg',
-  },
-  {
-    id: 'u2',
-    name: 'Emily Thompson',
-    avatar: 'user-2.jpg',
-  },
-  {
-    id: 'u3',
-    name: 'Marcus Johnson',
-    avatar: 'user-3.jpg',
-  },
-  {
-    id: 'u4',
-    name: 'David Miller',
-    avatar: 'user-4.jpg',
-  },
-  {
-    id: 'u5',
-    name: 'Priya Patel',
-    avatar: 'user-5.jpg',
-  },
-  {
-    id: 'u6',
-    name: 'Arjun Singh',
-    avatar: 'user-6.jpg',
-  },
-];
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+}
