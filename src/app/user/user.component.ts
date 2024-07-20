8d2f7adb1fec829961f681475f77c3765663763e
ali-ashhar <aliashhar021@gmail.com>
2024-07-20 21:37:51 +0330

    remove all lerning works and ready project to emplipent goals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,5 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,17 +7,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-
   // get imagePath() {
   //   // a getter function to dynamic path value
   //   return 'assets/users/' + this.selectedUser.avatar;
   // }
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
   // function for click event
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
+  onSelectUser() {}
 }
