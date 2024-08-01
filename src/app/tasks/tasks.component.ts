613f9e9bd0613fd507ec1dc0aeac59fa881e648b
ali-ashhar <aliashhar021@gmail.com>
2024-08-01 14:26:38 +0330

    use dummy data and show that on task & tasks comp

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { take } from 'rxjs';
 import { TaskComponent } from './task/task.component';
+import { dummyTasks } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -10,5 +10,12 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+  tasks = dummyTasks;
+  @Input({ required: true }) userId!: string;
+
   @Input({ required: true }) name!: string;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
