430d377399e04b0179de54f4dee4e17780ef8214
ali-ashhar <aliashhar021@gmail.com>
2024-08-02 19:20:26 +0330

    v42 add new-task component add event listiner to add task button show new comp if it was clicked

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  namee?: string;
+  onAddTask(name: string) {
+    this.namee = name;
+  }
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,22 +1,25 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from '../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   tasks = dummyTasks;
   @Input({ required: true }) userId!: string;
-
   @Input({ required: true }) name!: string;
-
+  isAddTask = false;
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
+  }
+  onStartAddTask() {
+    this.isAddTask = true;
   }
   onComplite(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id)!;
