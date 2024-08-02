8482782dd54be88064d4d133544efe3a2cf28e5c
ali-ashhar <aliashhar021@gmail.com>
2024-08-02 20:19:47 +0330

    v44 add ngModel witch use two way binding on inputs

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
   @Output() cancel = new EventEmitter();
 
   onCancel() {
